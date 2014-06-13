bfef7a6b59f0f958ca1c4484a6a6b9f74ae33a47
Ryan Kelly <ryan.kelly@clear.net.nz>
2014-06-13 18:03:05 +1200

    added all asset folders and basic header and footer .php

---
+++ views/footer.php
@@ -0,0 +1,5 @@
+		
+	</div>
+<script src="assets/js/script.js"></script>
+</body>
+</html>
---
+++ views/header.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>hav.oc</title>
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+	<div class="container">
