97e1b4ab20599f89a55539c3ccc6ebd5b9ba83bb
Russell Wilmshurst <plane-rusty@hotmail.com>
2014-06-24 18:11:47 +1200

    fixed login and rega

--- public/register.php
+++ public/register.php
@@ -1,20 +1,24 @@
 <?php
 
 require_once '../libraries/database.lib.php';
-require_once '../models/category.collection.php';
 require_once '../libraries/form.class.php';
-require_once '../models/product.collection.php';
 require_once '../libraries/login.class.php';
+require_once '../libraries/hash.class.php';
 require_once '../libraries/model.lib.php';
 require_once '../libraries/collection.lib.php';
+require_once '../models/category.collection.php';
+require_once '../models/product.collection.php';
 require_once '../models/user.model.php';
 
 if ($_POST) {
 	$user = new User();
 
 	$user->username = $_POST['username'];
-	$user->password = $_POST['password'];
+	$user->password = Hash::make_password($_POST['password']);
+	$user->salt     = Hash::salt();
 	$user->save();
+
+	
 }
 
 include '../views/header.php';
